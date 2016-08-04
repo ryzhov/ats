b338d113bddf2d37c72bbc08aceb9f5c93f7aba0
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2016-08-04 18:46:59 +0000

    first commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_URL','host=localhost dbname=ats0 user=app password=pass4APP');
+define('DOMAIN','ats0.uiptel.com');
+define('GUEST_USER_ID', 255);
+define('SESS_NAME', 'sessid');
+
+?>
